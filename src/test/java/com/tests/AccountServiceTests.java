4005dbf091be4ad788bb0fb8d75ed7a711ce84dd
Jeremy Critchley <jeremycritchley@Jeremys-MacBook-Pro.local>
2020-11-04 22:56:09 -0500

    naive service tests

--- src/test/java/com/tests/AccountServiceTests.java
+++ src/test/java/com/tests/AccountServiceTests.java
@@ -1,41 +1,55 @@
 package com.tests;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
 
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.models.Account;
 import com.services.AccountService;
 
 public class AccountServiceTests {
-	private static final AccountService myAccService = new AccountService();
 	
-	@BeforeClass
-	public void before() {
-		
-	}
+	private static final AccountService accServ = new AccountService();
+	private Account acc = new Account(1, 1, 0.0, 1);;
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
 	
 	@Test
 	public void testWithdraw() {
-		
+		assertTrue(accServ.withdraw(acc, 100));
 	}
 	
 	@Test
 	public void testWithdrawFail() {
-		
+		assertFalse(accServ.withdraw(acc, 1000000));
 	}
 	
 	@Test
 	public void testDeposit() {
-		
+		assertTrue(accServ.deposit(acc, 100));
 	}
 	
 	@Test
 	public void testTransferSuccess() {
-		
+		Account a = acc;
+		a.setUserId(2);
+		a.setAccId(2);
+		assertTrue(accServ.transfer(acc, a, 100));
 	}
 	
 	@Test
 	public void testTransferFail() {
-		
+		Account a = acc;
+		a.setUserId(2);
+		a.setAccId(2);
+		assertFalse(accServ.transfer(acc, a, 1000000));
 	}
 	
 	
--- src/test/java/com/tests/AdminServiceTests.java
+++ src/test/java/com/tests/AdminServiceTests.java
@@ -1,9 +1,41 @@
 package com.tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.models.Account;
+import com.models.User;
+import com.services.AdminService;
+
 public class AdminServiceTests {
 
+	private static final AdminService adServ = new AdminService();
+	
+	@Test
+	public void testRemoveUserFail() {
+		User u = new User(0, "fake", "fakerson", "fake", "fakefake", "fake@fake", "fake");
+		assertFalse(adServ.removeUser(u));
+	}
+	
+	@Test
+	public void testRemoveUserSuccess() {
+		User u = new User(3, "remove", "remove", "remove", "remove", "remove", "remove");
+		assertTrue(adServ.removeUser(u));
+	}
+	
+	@Test
+	public void testRemoveAccountFail() {
+		Account a = new Account(0, 0, 0.0, 0);
+		assertFalse(adServ.removeAccount(a));
+	}
+	
+	@Test
+	public void testRemoveAccountSuccess() {
+		Account a = new Account(3, 0, 0.0, 0);
+		assertTrue(adServ.removeAccount(a));
+	}
+	
+	
 }
